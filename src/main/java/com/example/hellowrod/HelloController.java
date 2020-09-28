4012db4b408fe91e5303d1b2d6225e4070178284
韩可迅 <2215396405@qq.com>
2020-09-28 17:46:40 +0800

    Initial commit

---
+++ src/main/java/com/example/hellowrod/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.hellowrod;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author kxhan
+ * @createTime 2020/09/28
+ */
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("hello")
+    public String hello() {
+        System.out.println("hello");
+        return "hello";
+    }
+}
---
+++ src/test/java/com/example/hellowrod/HelloWrodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellowrod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWrodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
