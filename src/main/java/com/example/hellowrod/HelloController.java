3181b6fc9cf9246035510a85ef362ba5d53f87ea
韩可迅 <2215396405@qq.com>
2020-10-19 14:38:31 +0800

    aaa

--- src/main/java/com/example/hellowrod/HelloController.java
+++ src/main/java/com/example/hellowrod/HelloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("hello")
+    @RequestMapping("/hello")
     public String hello() {
         System.out.println("hello");
         return "hello";
